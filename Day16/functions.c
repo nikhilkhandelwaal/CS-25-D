e009d224207d08f1f094d896359f643a3b4ffe0b
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2025-11-03 15:16:35 +0530

    loops

---
+++ Day16/functions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Function Definations
+void printHello() {
+    printf("Hello World\n");
+}
+
+
+void hello() {
+    printf("Welcome to NCU\n");
+    printHello();
+}
+
+
+int main() {
+    printHello();
+    return 0;
+}
+
+/*
+function can take values(arguments) and return result
+----------------------------
+  a+b =30
+        a-b = -10
+        a*b= 200
+        a/b = 0.5
+
+----------------------------
+    int a=10;
+    int b=20;
+
+    a+b =30
+        a-b = -10
+        a*b= 200
+        a/b = 0.5
+
+    int x=15;
+    int y=25;
+    x+y=40;
+*/
